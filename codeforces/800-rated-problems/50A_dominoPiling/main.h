42b7602a91364e79fbc9e6a2d027fce8b912bda1
Sangram <180935703+sangramofc@users.noreply.github.com>
2026-05-02 14:01:57 +0530

    CF: solved 1992-A-Only Pluses(greedy,math)

---
+++ codeforces/800-rated-problems/50A_dominoPiling/main.h
@@ -0,0 +1,16 @@
+//
+// Created by sangr on 02-05-2026.
+//
+
+#ifndef COMPETITIVEPROGRAMMING_MAIN_H
+#define COMPETITIVEPROGRAMMING_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //COMPETITIVEPROGRAMMING_MAIN_H
